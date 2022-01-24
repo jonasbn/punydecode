ab4be9a3d93513e8b7c34f400e44dd5b0ad03c0c
Jonas Brømsø <jonasbn@users.noreply.github.com>
2022-01-24 20:44:42 +0100

    Ability to read from STDIN (#1)
    * Added our little executabilito to gitignore
    
    * Added ability to read from STDIN

--- punydecode.go
+++ punydecode.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
 
 	"golang.org/x/net/idna"
 )
 
 func main() {
-	argStr := os.Args[1] // we only take a single parameter and
+	argsWithoutProg := os.Args[1:]
 
-	str, err := idna.ToUnicode(argStr)
+	var punycodeString string
+
+	if len(argsWithoutProg) <= 0 {
+		scanner := bufio.NewScanner(os.Stdin)
+
+		for scanner.Scan() {
+			punycodeString = scanner.Text()
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+
+	} else {
+		punycodeString = os.Args[1] // we only take a single parameter, the string to decode
+	}
+
+	decodedString, err := idna.ToUnicode(punycodeString)
 
 	if err == nil {
-		fmt.Printf("%s\n", str)
+		fmt.Printf("%s\n", decodedString)
 	}
 }
