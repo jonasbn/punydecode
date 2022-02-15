8f43a574426e71d3f23fce5c01e5a562af9282ed
jonasbn <jonasbn@gmail.com>
2022-02-15 09:29:57 +0100

    Implmenting first shot at unit-tests

---
+++ main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"testing"
+)
+
+func TestArguments(T *testing.T) {
+	// We manipuate the Args to set them up for the testcases
+	// After this test we restore the initial args
+	oldArgs := os.Args
+	defer func() { os.Args = oldArgs }()
+
+	cases := []struct {
+		Name           string
+		Args           []string
+		ExpectedExit   int
+		ExpectedOutput string
+	}{
+		{"no arguments", []string{""}, 1, ""},
+		{"single ASCII argument", []string{"test"}, 0, "test\n"},
+		{"single punycode encoded and lower cased argument", []string{"xn--kdplg-orai3l"}, 0, "kødpålæg\n"},
+		{"single punycode encoded and lower cased argument", []string{"xn--MASSEDELGGELSESVBEN-5ebm60b"}, 0, "MASSEØDELÆGGELSESVÅBEN\n"},
+		{"multiple punycode encoded and lower and upper cased arguments", []string{"xn--kdplg-orai3l", "xn--BLBRGRD-3pak7p"}, 0, "kødpålæg BLÅBÆRGRØD\n"},
+	}
+
+	for _, tc := range cases {
+		// this call is required because otherwise flags panics,
+		// if args are set between flag.Parse call
+		flag.CommandLine = flag.NewFlagSet(tc.Name, flag.ExitOnError)
+		// we need a value to set Args[0] to cause flag begins parsing at Args[1]
+		os.Args = append([]string{tc.Name}, tc.Args...)
+		actualExit := realMain()
+		if tc.ExpectedExit != actualExit {
+			T.Errorf("Wrong exit code for args: %v, expected: %v, got: %v",
+				tc.Args, tc.ExpectedExit, actualExit)
+		}
+	}
+
+}
